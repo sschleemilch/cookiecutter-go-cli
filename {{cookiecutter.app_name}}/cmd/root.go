53bfbf9f565ec1e1759ed0625b9e9c5747582638
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2024-09-11 17:10:24 +0200

    Formatting

--- {{cookiecutter.app_name}}/cmd/root.go
+++ {{cookiecutter.app_name}}/cmd/root.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 	"os"
 	"strings"
-
-    "{{ cookiecutter.module_name }}/version"
-    "github.com/rs/zerolog"
-    "github.com/rs/zerolog/log"
+	"{{ cookiecutter.module_name }}/version"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -18,47 +17,47 @@ var rootCmd = &cobra.Command{
 	Use:   "{{ cookiecutter.bin_name }}",
 	Short: "A brief description of your application",
 	Long: `A longer description`,
-    Version: version.Version,
-    PersistentPreRun: func(cmd *cobra.Command, args []string) {
-        initializeLogger(viper.GetString("log.level"), viper.GetBool("log.caller"), viper.GetString("log.file"))
-    },
-    {% if cookiecutter.subcommands != "y" %}
-    Run: func(cmd *cobra.Command, args []string) {
-        log.Info().Msg("Running root info")
-        log.Debug().Msg("Running root debug")
-    },
-    {% endif %}
+	Version: version.Version,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		initializeLogger(viper.GetString("log.level"), viper.GetBool("log.caller"), viper.GetString("log.file"))
+	},
+{% if cookiecutter.subcommands != "y" %}
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info().Msg("Running root info")
+		log.Debug().Msg("Running root debug")
+	},
+{% endif %}
 }
 
 func initializeLogger(logLevel string, logCaller bool, logFile string) {
-    level, err := zerolog.ParseLevel(strings.ToLower(logLevel))
-    if err != nil {
-        log.Fatal().Err(err).Msg("Invalid log level")
-    }
-    zerolog.SetGlobalLevel(level)
+		level, err := zerolog.ParseLevel(strings.ToLower(logLevel))
+		if err != nil {
+			log.Fatal().Err(err).Msg("Invalid log level")
+		}
+		zerolog.SetGlobalLevel(level)
 
-    consoleWriter := &zerolog.ConsoleWriter{Out: os.Stderr}
+		consoleWriter := &zerolog.ConsoleWriter{Out: os.Stderr}
 
-    var logFileFd *os.File
-    if logFile != "" {
-        logFileFd, err = os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-        if err != nil {
-            log.Fatal().Err(err).Msgf("Could not open logfile: %s", logFile)
-        }
-    }
+		var logFileFd *os.File
+		if logFile != "" {
+			logFileFd, err = os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+			if err != nil {
+				log.Fatal().Err(err).Msgf("Could not open logfile: %s", logFile)
+			}
+		}
 
-    if logFileFd != nil {
-        log.Logger = zerolog.New(zerolog.MultiLevelWriter(consoleWriter, logFileFd))
-    } else {
-        log.Logger = zerolog.New(zerolog.MultiLevelWriter(consoleWriter))
-    }
+		if logFileFd != nil {
+			log.Logger = zerolog.New(zerolog.MultiLevelWriter(consoleWriter, logFileFd))
+		} else {
+			log.Logger = zerolog.New(zerolog.MultiLevelWriter(consoleWriter))
+		}
 
-    if logCaller {
-        log.Logger = log.With().Caller().Logger()
-    }
+		if logCaller {
+			log.Logger = log.With().Caller().Logger()
+		}
 
-    log.Logger = log.With().Timestamp().Logger()
-}
+		log.Logger = log.With().Timestamp().Logger()
+	}
 
 func Execute() {
 	err := rootCmd.Execute()
@@ -69,13 +68,13 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-    rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "{{ cookiecutter.config_type }} config file (default: $HOME/{{ cookiecutter.default_config_prefix }}.{{ cookiecutter.config_type }})")
-    rootCmd.PersistentFlags().String("log-level", "info", "Set the log level (debug, info, warn, error, fatal, panic) (default: info)")
-    rootCmd.PersistentFlags().String("log-file", "", "Write logs in json format to this file (default: '')")
-    rootCmd.PersistentFlags().Bool("log-caller", false, "Include the caller file and line number (default: false)")
-    viper.BindPFlag("log.level", rootCmd.PersistentFlags().Lookup("log-level"))
-    viper.BindPFlag("log.file", rootCmd.PersistentFlags().Lookup("log-file"))
-    viper.BindPFlag("log.caller", rootCmd.PersistentFlags().Lookup("log-caller"))
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "{{ cookiecutter.config_type }} config file (default: $HOME/{{ cookiecutter.default_config_prefix }}.{{ cookiecutter.config_type }})")
+	rootCmd.PersistentFlags().String("log-level", "info", "Set the log level (debug, info, warn, error, fatal, panic) (default: info)")
+	rootCmd.PersistentFlags().String("log-file", "", "Write logs in json format to this file (default: '')")
+	rootCmd.PersistentFlags().Bool("log-caller", false, "Include the caller file and line number (default: false)")
+	viper.BindPFlag("log.level", rootCmd.PersistentFlags().Lookup("log-level"))
+	viper.BindPFlag("log.file", rootCmd.PersistentFlags().Lookup("log-file"))
+	viper.BindPFlag("log.caller", rootCmd.PersistentFlags().Lookup("log-caller"))
 }
 
 func initConfig() {
@@ -85,11 +84,11 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 		viper.AddConfigPath(home)
-        viper.SetConfigType("{{ cookiecutter.config_type }}")
+		viper.SetConfigType("{{ cookiecutter.config_type }}")
 		viper.SetConfigName("{{ cookiecutter.default_config_prefix }}")
 	}
-    viper.SetEnvPrefix("{{ cookiecutter.env_prefix }}")
-    viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.SetEnvPrefix("{{ cookiecutter.env_prefix }}")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err == nil {
--- {{cookiecutter.app_name}}/cmd/version.go
+++ {{cookiecutter.app_name}}/cmd/version.go
@@ -1,20 +1,20 @@
 package cmd
 
 import (
-    "{{ cookiecutter.module_name }}/version"
+	"{{ cookiecutter.module_name }}/version"
 	"github.com/spf13/cobra"
-    "fmt"
+	"fmt"
 )
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Detailed version information",
 	Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Version:", version.Version)
-        fmt.Println("Go Version:", version.GoVersion)
+		fmt.Println("Version:", version.Version)
+		fmt.Println("Go Version:", version.GoVersion)
 		fmt.Println("Build Date:", version.BuildDate)
 		fmt.Println("Git Commit:", version.GitCommit)
-        fmt.Println("OS / Arch:", version.OsArch)
+		fmt.Println("OS / Arch:", version.OsArch)
 	},
 }
 
