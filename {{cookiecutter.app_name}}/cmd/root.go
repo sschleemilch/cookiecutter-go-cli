c40200d51e8e73ba722db04ee66a9f83cef3e029
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2025-01-30 15:19:29 +0100

    Update

--- {{cookiecutter.app_name}}/cmd/root.go
+++ {{cookiecutter.app_name}}/cmd/root.go
@@ -3,12 +3,13 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strings"
-	"{{ cookiecutter.module_name }}/version"
-	"github.com/rs/zerolog"
+
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"{{ cookiecutter.module_name }}/logger"
+	"{{ cookiecutter.module_name }}/version"
 )
 
 var cfgFile string
@@ -29,36 +30,6 @@ var rootCmd = &cobra.Command{
 {% endif %}
 }
 
-func initializeLogger(logLevel string, logCaller bool, logFile string) {
-		level, err := zerolog.ParseLevel(strings.ToLower(logLevel))
-		if err != nil {
-			log.Fatal().Err(err).Msg("Invalid log level")
-		}
-		zerolog.SetGlobalLevel(level)
-
-		consoleWriter := &zerolog.ConsoleWriter{Out: os.Stderr}
-
-		var logFileFd *os.File
-		if logFile != "" {
-			logFileFd, err = os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-			if err != nil {
-				log.Fatal().Err(err).Msgf("Could not open logfile: %s", logFile)
-			}
-		}
-
-		if logFileFd != nil {
-			log.Logger = zerolog.New(zerolog.MultiLevelWriter(consoleWriter, logFileFd))
-		} else {
-			log.Logger = zerolog.New(zerolog.MultiLevelWriter(consoleWriter))
-		}
-
-		if logCaller {
-			log.Logger = log.With().Caller().Logger()
-		}
-
-		log.Logger = log.With().Timestamp().Logger()
-	}
-
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -68,24 +39,17 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	// Config
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "{{ cookiecutter.config_type }} config file")
+
+	// Logging
 	rootCmd.PersistentFlags().String("log-level", "info", "Set the log level (debug, info, warn, error, fatal, panic)")
-	rootCmd.PersistentFlags().String("log-file", "", "Write logs in json format to this file")
-	rootCmd.PersistentFlags().Bool("log-caller", false, "Include the caller file and line number")
 	viper.BindPFlag("log.level", rootCmd.PersistentFlags().Lookup("log-level"))
+
+	rootCmd.PersistentFlags().String("log-file", "", "Write logs in json format to this file")
 	viper.BindPFlag("log.file", rootCmd.PersistentFlags().Lookup("log-file"))
+
+	rootCmd.PersistentFlags().Bool("log-caller", false, "Include the caller file and line number")
 	viper.BindPFlag("log.caller", rootCmd.PersistentFlags().Lookup("log-caller"))
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	}
-	viper.SetEnvPrefix("{{ cookiecutter.env_prefix }}")
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
