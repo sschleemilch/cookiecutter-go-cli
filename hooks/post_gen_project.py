71fdf58c3faa2f2302d16aa4e1b0630ccc05dc9e
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2025-11-06 05:36:34 +0100

    fix: simplified layout. Using 'internal'. No Subcommands in template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python
-import os
 import subprocess
+from pathlib import Path
 
-PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
+def get_deps() -> list[str]:
+    mod = Path("go.mod").read_text()
 
-def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+    deps: set[str] = set()
+
+    in_require = False
+    for line in mod.splitlines():
+        line = line.strip()
+        if line.startswith("require ("):
+            in_require = True
+            continue
+        if not in_require:
+            continue
+        if line.startswith(")"):
+            in_require = False
+            continue
+        if "// indirect" in line:
+            continue
+
+        deps.add(line.strip().split()[0])
+
+    return list(deps)
 
 
 if __name__ == "__main__":
     if "{{ cookiecutter.git }}" == "y":
-        subprocess.check_call(["git", "init", "-b", "main"], cwd=PROJECT_DIRECTORY)
+        _ = subprocess.check_call(["git", "init", "-b", "main"])
     else:
-        remove_file(".gitignore")
-        remove_file(".pre-commit-config.yaml")
+        Path(".gitignore").unlink()
+        Path(".pre-commit-config.yaml").unlink()
+
+    _ = subprocess.check_call(["go", "mod", "tidy"])
+
+    for dep in get_deps():
+        _ = subprocess.check_call(["go", "get", dep])
--- {{cookiecutter.app_name}}/main.go
+++ {{cookiecutter.app_name}}/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "{{ cookiecutter.module_name }}/cmd"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"{{ cookiecutter.module_name }}/internal/logger"
+	"{{ cookiecutter.module_name }}/version"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:     "{{ cookiecutter.bin_name }}",
+	Short:   "A brief description of your application",
+	Long:    `A longer description`,
+	Version: version.GetVersion().Details(),
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logger.Init(viper.GetString("log.level"), viper.GetBool("log.caller"), viper.GetString("log.file"), viper.GetBool("log.json"))
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info().Msg("Running")
+	},
+}
 
 func main() {
-	cmd.Execute()
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func bindFlag(key string, flag string) {
+	if err := viper.BindPFlag(key, rootCmd.PersistentFlags().Lookup(flag)); err != nil {
+		log.Warn().Err(err).Msgf("[Viper] Could not bind flag: %s", flag)
+	}
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	}
+	viper.SetEnvPrefix("{{ cookiecutter.env_prefix }}")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Config
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "{{ cookiecutter.config_type }} config file")
+
+	// Logging
+	rootCmd.PersistentFlags().String("log-level", "info", "Set the log level (debug, info, warn, error, fatal, panic)")
+	bindFlag("log.level", "log-level")
+
+	rootCmd.PersistentFlags().String("log-file", "", "Write logs in json format to this file")
+	bindFlag("log.file", "log-file")
+
+	rootCmd.PersistentFlags().Bool("log-caller", false, "Include the caller file and line number")
+	bindFlag("log.caller", "log-caller")
+
+	rootCmd.PersistentFlags().Bool("log-json", false, "Log as json messages")
+	bindFlag("log.json", "log-json")
 }
